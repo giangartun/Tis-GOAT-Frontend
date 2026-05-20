8a5735b070e4ec9b5d1ecbfd7f5c8751901f4312
CBOLINHA <itsebastianex1@gmail.com>
2026-05-20 10:58:57 -0400

    Arreglo despues del Merge

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import AnadirEnlaces from "./Pages/AnadirEnlaces";
 import PerfilUsuario from "./Pages/perfilusuario";
 import PrivacidadPortafolio from "./Pages/PrivacidadPortafolio";
 import PerfilPublico from "./Pages/PerfilPublico";
+import { ResetPassword } from "./Pages/RestablecerContraseña";
 
 function App() {
   return (

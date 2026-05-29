c4b752ed5ad71255535c20d2540b69a7de4d8d04
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2026-05-29 01:16:56 -0400

    fix : arreglo de harcodeo en vista publicas

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: (import.meta as any)?.env?.VITE_API_URL?.replace(/\/$/, '') ||
+  'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
       },
